a0761de2b88a901a011e0e4f9b75f20a820ad9f3
nagaganesh05 <somanagaganesh@gmail.com>
2024-11-15 12:02:59 +0530

    messages

---
+++ function.js
@@ -0,0 +1,17 @@
+
+// push in function
+function pusharray(arr,a,b){
+    arr.push(a,b);
+    console.log(arr)
+}
+const arr = [1,2,5,6];
+pusharray(arr,3,4)
+
+
+// pop in function
+function poparray(arr1,a,b){
+    arr.pop();
+    console.log(arr1)
+}
+const arr1 = [1,2,5,6];
+poparray(arr1,3,4)
